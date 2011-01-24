3e8293ba607b2b0cfd0b985827cda8b4a518e0f3
Jan Kaiser <jan.kaiser@emercs.com>
2011-01-24 15:43:24 +0100

    Routes korrekt gesetzt: Jetzt Zugriff auf / sttat /pages

--- app/controllers/webservice_controller.rb
+++ app/controllers/webservice_controller.rb
@@ -2,12 +2,11 @@ class WebserviceController < ApplicationController
   def req
 
       # Dummy-Request: Meldet immer erfolgreich
-      myRequest = {
+      render :json => {
           :id => params[:id],
           :statuscode => 200,
           :par => params[:data]
       }
-      render :json => myRequest
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  match '/report', :to => 'pages#report'
+  match '/home', :to => 'pages#home'
+
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -55,7 +59,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
